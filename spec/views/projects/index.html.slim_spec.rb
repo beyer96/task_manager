969389bd706a64724a9fcaa2b0d29613d7115a36
Lukas Beyer <lukas.beyer@juicymo.cz>
2024-08-04 19:35:38 +0200

    Fix view specs

--- spec/views/projects/index.html.slim_spec.rb
+++ spec/views/projects/index.html.slim_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "projects/index", type: :view do
 
   it "renders a list of projects" do
     render
-    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
+    cell_selector = 'tr>td'
     assert_select cell_selector, text: Regexp.new("Project".to_s), count: 2
     assert_select cell_selector, text: Regexp.new(1.to_s), count: 2
   end
--- spec/views/tasks/index.html.slim_spec.rb
+++ spec/views/tasks/index.html.slim_spec.rb
@@ -11,9 +11,8 @@ RSpec.describe "tasks/index", type: :view do
   it "renders a list of tasks" do
     render
 
-    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
+    cell_selector = 'tr>td'
     assert_select cell_selector, text: Regexp.new("Task 1"), count: 2
     assert_select cell_selector, text: Regexp.new("Description"), count: 2
-    assert_select cell_selector, text: Regexp.new(false.to_s), count: 2
   end
 end
