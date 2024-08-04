0e4524f9b96124c7aa63416ed039213932a651e0
Lukas Beyer <lukas.beyer@juicymo.cz>
2024-08-04 23:05:25 +0200

    Fix behavior of tooltips when rendering turbo streams

--- app/javascript/tooltips.js
+++ app/javascript/tooltips.js
@@ -1,7 +1,27 @@
 import { Tooltip } from "bootstrap";
 
-document.addEventListener("turbo:load", () => {
-  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+let tooltipList;
 
-  [...tooltipTriggerList].map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl));
+document.addEventListener("turbo:load", () => {
+  tooltipList = initTooltips();
 });
+
+document.addEventListener("turbo:before-stream-render", async event => {
+  const originalRender = event.detail.render;
+
+  event.detail.render = function (streamElement) {
+    originalRender(streamElement);
+    document.dispatchEvent(new Event("after-turbo-stream-render", { streamElement }));
+  };
+});
+
+document.addEventListener("after-turbo-stream-render", streamElement => {
+  tooltipList.forEach(tooltip => tooltip.hide());
+  tooltipList = [...tooltipList, ...initTooltips(streamElement.templateContent)];
+});
+
+const initTooltips = (domTarget = document) => {
+  const tooltipElements = domTarget.querySelectorAll('[data-bs-toggle="tooltip"]');
+  
+  return [...tooltipElements].map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl));
+}
