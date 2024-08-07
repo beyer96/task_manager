bfa4ce7856ead63a1a329013c5cc2ea7733ed296
Lukas Beyer <lukas.beyer@juicymo.cz>
2024-08-07 20:59:36 +0200

    Replace state in browser history when using search

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,10 +2,20 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["input"];
-  
-  clearSearchInput() {
-    setTimeout(() => {
-      this.inputTarget.value = "";
-    }, 0);
+
+  connect() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const queryParam = urlParams.get("query");
+
+    queryParam && (this.inputTarget.value = queryParam);
+  }
+
+  replaceState() {
+    const { origin, pathname, search } = window.location;
+    const inputValue = this.inputTarget.value;
+    const newParams = new URLSearchParams(search);
+
+    newParams.set("query", inputValue);
+    history.replaceState(null, null, `${origin}${pathname}?${newParams}`);
   }
 }
