e2102caf3799e6c877b4b2126cae0e15192390ef
Lukas Beyer <lukas.beyer@juicymo.cz>
2024-08-08 09:43:21 +0200

    Fix view test

--- spec/views/projects/index.html.slim_spec.rb
+++ spec/views/projects/index.html.slim_spec.rb
@@ -2,14 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "projects/index", type: :view do
   let(:user) { create(:user) }
+  let(:projects) {
+    create_list(:project, 2, user:) do |project, index|
+      project.position = index + 100
+      project.save!
+    end
+  }
   before(:each) do
-    assign(:projects, create_list(:project, 2, user:))
+    assign(:projects, projects)
   end
 
   it "renders a list of projects" do
     render
     cell_selector = 'tr>td'
     assert_select cell_selector, text: Regexp.new("Project".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(1.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(100.to_s), count: 1
+    assert_select cell_selector, text: Regexp.new(101.to_s), count: 1
   end
 end
